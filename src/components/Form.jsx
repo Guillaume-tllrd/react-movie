0de925bb27e6a912a8c192205c3b437e1d66ef69
Guillaume TILLARD <guillaumetillard10@gmail.com>
2024-08-03 15:44:27 +0200

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div className='form-component'>
+            <div className="form-container">
+                <form action="">
+                    <input type="text" placeholder="Entrez le titre d'un film" id='search-input'  />
+                    <input type="submit" value="Rechercher"/>
+                </form>
+                <div className="btn-sort-container">
+                    <div className="btn-sort" id="goodToBad">Top <span>-></span></div>
+                    <div className="btn-sort" id="badToGood">Flop <span>-></span></div>
+                </div>
+            </div>
+            <div className="result">RESULTAT</div>
+        </div>
+    );
+};
+
+export default Form;
